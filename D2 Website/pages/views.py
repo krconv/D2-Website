77196e47f950508b044206d165a2c1a51325c750
Kodey Converse <kodey@krconv.com>
2016-06-17 14:55:26 -0400

    Add the Meet Us page, adjust styling of footer.

--- D2 Website/pages/views.py
+++ D2 Website/pages/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, "pages/News.html");
+    return render(request, "pages/Home.html");
 
 def links_page(request):
     return render(request, "pages/UsefulLinks.html");
+
+def meet_us_page(request):
+    return render(request, "pages/MeetUs.html");
 
 def contact_page(request):
     return render(request, "pages/ContactUs.html");
